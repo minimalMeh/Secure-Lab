119ff607cb3a7b1729c1ba9187b82ce603eb2604
minimalMeh <dasha.osd@gmail.com>
2020-10-06 13:41:13 +0300

    add check values for update

--- SecureLab.Application/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
+++ SecureLab.Application/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using SecureLab.Persistence;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace SecureLab.Application.Users.Commands.UpdateUser
 {
@@ -23,17 +24,30 @@ namespace SecureLab.Application.Users.Commands.UpdateUser
             var user = await _context.Users.FirstOrDefaultAsync(e => e.Id == request.Id);
             if (user != null)
             {
-                user.UserName = request.UserName;
-                user.PhoneNumber = request.PhoneNumber;
-                user.Information = request.Information;
-                user.AllowedRoomGroups = request.AllowedRoomGroups.ToArray();
-                user.AllowedRooms = request.AllowedRooms.ToArray();
+                user.UserName = this.UpdateField(user.UserName, request.UserName);
+                user.PhoneNumber = this.UpdateField(user.PhoneNumber, request.PhoneNumber);
+                user.Information = this.UpdateField(user.Information, request.Information);
+                user.AllowedRoomGroups = this.UpdateField(user.AllowedRoomGroups, request.AllowedRoomGroups?.ToArray());
+                user.AllowedRooms = this.UpdateField(user.AllowedRooms, request.AllowedRooms?.ToArray());
             }
 
             _context.Users.Update(user);
             await _context.SaveChangesAsync(cancellationToken);
 
             return new UserResponse(user.Id);
+        }
+
+        /// <summary>
+        /// Check wheter field changed from previous value.
+        /// </summary>
+        /// <returns>Returns updated value or previous value.</returns>
+        private T UpdateField<T> (T oldValue, T newValue)
+        {
+            if (newValue != null && !EqualityComparer<T>.Default.Equals(oldValue, newValue))
+            {
+                return newValue;
+            }
+            return oldValue;
         }
     }
 }
