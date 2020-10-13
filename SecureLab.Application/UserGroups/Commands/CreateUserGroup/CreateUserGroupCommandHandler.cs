9c54984f9840123b76216826749e50f616cee40c
minimalMeh <dasha.osd@gmail.com>
2020-10-13 17:48:57 +0300

    add create user group command

---
+++ SecureLab.Application/UserGroups/Commands/CreateUserGroup/CreateUserGroupCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using SecureLab.Domain.Entities;
+using SecureLab.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SecureLab.Application.UserGroups.Commands.CreateUserGroup
+{
+    class CreateUserGroupCommandHandler : IRequestHandler<CreateUserGroupCommand, UserGroupResponse>
+    {
+        private readonly IMediator _mediator;
+        private readonly SecureLabDbContext _context;
+
+        public CreateUserGroupCommandHandler(IMediator mediator, SecureLabDbContext context)
+        {
+            this._mediator = mediator;
+            this._context = context;
+        }
+
+        public async Task<UserGroupResponse> Handle(CreateUserGroupCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new UserGroup
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Name
+            };
+
+            _context.UserGroups.Add(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new UserGroupResponse(entity.Id, entity.Title);
+        }
+    }
+}
---
+++ SecureLab.Application/UserGroups/Commands/CreateUserGroup/CreateUserGroupCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecureLab.Application.UserGroups.Commands.CreateUserGroup
+{
+    class CreateUserGroupCommandValidator : AbstractValidator<CreateUserGroupCommand>
+    {
+        public CreateUserGroupCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().NotNull();
+        }
+    }
+}
